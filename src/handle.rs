67b6c8813b5c364162bc7d1580f2ec2a46cb20ef
Yoimiya Naganohara <mi-123qwe@outlook.com>
2024-09-28 12:17:57 +0800

    Refactor release workflow: Update dependencies in Cargo.toml and Cargo.lock

---
+++ src/handle.rs
@@ -0,0 +1,177 @@
+use std::{
+    io::{stdin, Write},
+    path::PathBuf,
+    process::exit,
+    time,
+};
+
+use colored::Colorize;
+
+use crate::search_engine::{Search, SearchEngine};
+
+pub struct Handle {
+    welcome: String,
+    command: String,
+    engine: Search,
+}
+pub(crate) trait Handler {
+    fn new() -> Self;
+
+    fn welcome(&mut self);
+    fn input(&mut self);
+
+    fn handle(&mut self);
+}
+impl Handler for Handle {
+    fn new() -> Self {
+        Handle {
+            welcome: "
+        ███████╗ █████╗ ███████╗████████╗    ███████╗███████╗ █████╗ ██████╗ ███████╗██╗  ██╗
+        ██╔════╝██╔══██╗██╔════╝╚══██╔══╝    ██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝██║  ██║
+        ███████╗███████║███████╗   ██║       ███████╗█████╗  ███████║██████╔╝██║     ███████║
+        ██╔════╝██╔══██║╚════██║   ██║       ╚════██║██╔══╝  ██╔══██║██╔═══╝ ██║     ██╔══██║
+        ██║     ██║  ██║███████║   ██║       ███████║███████╗██║  ██║██║  ██╗███████╗██║  ██║
+        ╚═╝     ╚═╝  ╚═╝╚══════╝   ╚═╝       ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝"
+                .to_string(),
+            command: String::new(),
+            engine: Search::new(),
+        }
+    }
+
+    fn input(&mut self) {
+        print!("{}", ">".green());
+        std::io::stdout().flush().unwrap();
+        self.command.clear();
+        std::io::stdin().read_line(&mut self.command).unwrap();
+        self.command = self.command.trim().to_string();
+    }
+
+    fn handle(&mut self) {
+        match self.command.as_str() {
+            ":?" => {
+                println!("{}", "Usage:
+.     - Matches any character except a newline.
+^     - Matches the start of the string.
+$     - Matches the end of the string.
+*     - Matches 0 or more repetitions of the preceding pattern.
++     - Matches 1 or more repetitions of the preceding pattern.
+?     - Matches 0 or 1 repetition of the preceding pattern.
+{m,n} - Matches from m to n repetitions of the preceding pattern.
+[]    - Matches any single character in the brackets.
+|     - Matches either the pattern before or the pattern after the |.
+()    - Groups patterns.
+\nCommands:\n:C - Change directory\n:Q - Quit the application\n:U - Update the index (Add '*' to update given section)\n:D - Display search results\n:? - Show this help message".yellow());
+            }
+            ":C" => {
+                let mut path = String::new();
+                println!(
+                    "{}",
+                    format!(
+                        "Please enter the new directory path (current: {}). Type ':x' to cancel:",
+                        self.engine.get_root_dir().to_str().unwrap()
+                    )
+                    .yellow()
+                );
+                std::io::stdin().read_line(&mut path).unwrap();
+                path = path.trim().to_string();
+                if path == ":x" {
+                    return;
+                }
+                self.engine.set_root_dir(PathBuf::from(path));
+                self.engine.load_index();
+            }
+            ":Q" => exit(0),
+            ":U" => {
+                println!(
+                    "{}",
+                    "Generating index for the current directory...".yellow()
+                );
+
+                let start_time = time::SystemTime::now();
+                self.engine.generate_index();
+                let duration = start_time.elapsed().expect("Time went backwards");
+                println!(
+                    "{}",
+                    format!(
+                        "Index generation complete. Time taken: {:?}. Number of indexed items: {}",
+                        duration,
+                        self.engine.get_index().len()
+                    )
+                    .green()
+                );
+                self.engine.save_index();
+            }
+            _ => {
+                let start_time = time::SystemTime::now();
+                let data = self.engine.search(&self.command);
+                let duration = start_time.elapsed().expect("Time went backwards");
+
+                println!(
+                    "{}",
+                    format!(
+                        "Search completed. Time taken: {:?}. Number of results: {}\n",
+                        duration,
+                        data.len()
+                    )
+                    .green()
+                );
+                if data.is_empty() {
+                    return;
+                }
+
+                println!(
+                    "{}",
+                    "Tip: Use ':D' to display the search results.".yellow()
+                );
+                let mut buf = String::new();
+                stdin().read_line(&mut buf).unwrap();
+                buf = buf.trim().to_string();
+                if buf == ":D" {
+                    let mut counter = 0usize;
+                    for i in &data {
+                        println!("{} [{}]", counter, i.to_str().unwrap());
+                        counter += 1;
+                    }
+                    loop {
+                        println!(
+                            "{}",
+                            "Tip: Enter 'q' to quit, 'l<number>' to open the parent directory of the result, or just the number to open the result.".yellow()
+                        );
+                        let mut buf = String::new();
+                        stdin().read_line(&mut buf).unwrap();
+                        buf = buf.trim().to_string();
+                        let mut p = false;
+                        if buf == "q" {
+                            return;
+                        }
+                        if buf.contains('l') {
+                            buf = buf.trim_matches('l').to_string();
+                            p = true
+                        }
+                        if let Ok(index) = buf.parse::<usize>() {
+                            if let Some(mut dir) = data.get(index) {
+                                let path_buf = dir.parent().unwrap().to_path_buf();
+                                if p {
+                                    dir = &path_buf;
+                                }
+                                if let Err(e) = open::that(dir) {
+                                    eprintln!(
+                                        "{}",
+                                        format!("Failed to open directory: {}", e).red()
+                                    );
+                                }
+                            }
+                        } else {
+                            println!("{}", "Invalid input. Please enter a valid number.".red());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn welcome(&mut self) {
+        self.engine.load_index();
+        println!("{}", self.welcome.blue().bold())
+    }
+}
--- src/search_engine.rs
+++ src/search_engine.rs
@@ -1,238 +1,180 @@
-use crate::data::{Node, PathTree};
 use std::{
-    ffi::OsStr,
-    fs::{self, create_dir, exists, File},
+    fs::{read_dir, File},
+    io::{BufReader, BufWriter},
     path::PathBuf,
 };
 
-/// A trait that defines the basic operations for a search engine.
-pub(crate) trait SearchEngine {
-    /// Creates a new instance of the search engine.
-    fn new() -> Self;
+use regex::Regex;
 
-    /// Generates an index from the given root directory.
-    ///
-    /// # Arguments
-    ///
-    /// * `root_dir` - A `PathBuf` representing the root directory to index.
-    /// * `section` - A `&char` representing to update
-    fn generate_index(&mut self, root_dir: PathBuf, section: &char);
-    /// Searches for a keyword in the index.
-    ///
-    /// # Arguments
-    ///
-    /// * `keyword` - A `String` representing the keyword to search for.
-    ///
-    /// # Returns
-    ///
-    /// * `Result<&Vec<PathBuf>, ()>` - A result containing a reference to a vector of `PathBuf` if the search is successful, or an error `()` if the keyword is not found.
-    fn search(&self, keyword: &String) -> Result<Vec<PathBuf>, ()>;
-
-    /// Saves the current index to disk.
-    fn save_index(&mut self);
-
-    /// Loads an index from disk for a given section.
-    ///
-    /// # Arguments
-    ///
-    /// * `section` - A `char` representing the section to load.
-    fn load_index(&mut self, section: char);
-
-    /// Loads an index from disk for a given section.
-    ///
-    /// # Arguments
-    ///
-    /// * `section` - A `char`
-    /// Sets the part of the index to be used.    ///
-    /// # Arguments
-    ///
-    /// * `part` - A `char` representing the part of the index to set.
-    fn set_part(&mut self, part: char);
-
-    fn indexed(&self) -> usize;
+pub(crate) struct Search {
+    indexed_files: Vec<PathBuf>,
+    root_dir: PathBuf,
 }
-
+#[allow(dead_code)]
+pub trait SearchEngine {
+    fn new() -> Self;
+    fn generate_index(&mut self);
+    fn save_index(&self);
+    fn load_index(&mut self);
+    fn get_index(&self) -> &Vec<PathBuf>;
+    fn set_root_dir(&mut self, root_dir: PathBuf);
+    fn get_root_dir(&self) -> &PathBuf;
+    fn search(&self, key: &String) -> Vec<PathBuf>;
+}
 impl SearchEngine for Search {
-    fn new() -> Self {
-        Search {
-            index: Node::new(),
-            search_part: 'C',
-        }
-    }
+    fn generate_index(&mut self) {
+        // clear before new index added to indexed_files
+        self.indexed_files.clear();
 
-    fn search(&self, keyword: &String) -> Result<Vec<PathBuf>, ()> {
-        let mut search_results = Vec::new();
-        if !keyword.ends_with('?') {
-            let node = match self.index.get(&keyword) {
-                Some(x) => x,
-                None => {
-                    return Err(());
-                }
-            };
-            search_results.append(&mut node.val().clone());
-            return Ok(search_results);
-        }
-        let mut keyword = keyword.clone();
-        keyword.pop();
-        let node = match self.index.get(&keyword) {
-            Some(x) => x,
-            None => {
-                return Err(());
-            }
-        };
-
-        fn traverse_node(node: &Node, search_results: &mut Vec<PathBuf>) {
-            let mut path = node.val().clone();
-            search_results.append(&mut path);
-            if node.groups().len() == 0 {
+        fn traverse_index(current_path: &PathBuf, indexed: &mut Vec<PathBuf>) {
+            if current_path.metadata().is_err() {
                 return;
             }
-            for (_, sub_node) in node.groups() {
-                traverse_node(sub_node, search_results);
+
+            match read_dir(current_path) {
+                Ok(entries) => {
+                    for entry in entries {
+                        let entry = match entry {
+                            Ok(x) => x,
+                            Err(_) => {
+                                return;
+                            }
+                        };
+
+                        // is dir
+                        if entry.path().is_dir() {
+                            traverse_index(&entry.path(), indexed);
+                        } else if entry.path().is_file() {
+                            indexed.push(entry.path());
+                        }
+                    }
+                }
+                Err(_) => {
+                    return;
+                }
             }
         }
 
-        traverse_node(node, &mut search_results);
-        Ok(search_results)
+        traverse_index(&self.root_dir, &mut self.indexed_files);
     }
 
-    fn save_index(&mut self) {
-        if self.index.is_empty() {
-            return;
+    fn new() -> Self {
+        Search {
+            indexed_files: Vec::new(),
+            root_dir: PathBuf::from("C:\\"),
         }
-        if !exists(format!("index{}", self.search_part)).unwrap_or(false) {
-            if let Err(e) = create_dir(format!("index{}", self.search_part)) {
-                eprintln!("Failed to create directory: {}", e);
-            }
-        }
-        for (ch, node) in self.index.groups() {
-            let file = File::create(format!(
-                "index{}/data-{}{}",
-                self.search_part,
-                ch,
-                ch.is_uppercase()
-            ))
-            .expect("Failed to create file");
-            let mut writer = std::io::BufWriter::new(file);
-            bincode::serialize_into(&mut writer, node).expect("Failed to serialize data");
-        }
-        self.index.clear();
     }
 
-    fn load_index(&mut self, section: char) {
+    fn save_index(&self) {
+        let file = File::create(format!(
+            "index {}",
+            self.root_dir
+                .to_str()
+                .unwrap_or_default()
+                .replace("\\", "")
+                .replace(":", "")
+        ))
+        .expect("Fail to create file");
+
+        let writer = BufWriter::new(file);
+        if let Err(e) = bincode::serialize_into(writer, &self.indexed_files) {
+            eprintln!("Failed to serialize index: {}", e);
+        }
+    }
+
+    fn load_index(&mut self) {
         let file = match File::open(format!(
-            "index{}/data-{}{}",
-            self.search_part,
-            section,
-            section.is_uppercase()
+            "index {}",
+            self.root_dir
+                .to_str()
+                .unwrap_or_default()
+                .replace("\\", "")
+                .replace(":", "")
         )) {
             Ok(x) => x,
             Err(_) => {
-                self.index = Node::new();
+                self.indexed_files = Vec::new();
                 return;
             }
         };
-        let mut reader = std::io::BufReader::new(file);
-        self.index = bincode::deserialize_from(&mut reader).expect("Failed to deserialize data");
-    }
-    fn set_part(&mut self, part: char) {
-        self.search_part = part;
-    }
-    fn indexed(&self) -> usize {
-        self.index.len()
+        let reader = BufReader::new(file);
+        self.indexed_files = match bincode::deserialize_from(reader) {
+            Ok(x) => x,
+            Err(_) => Vec::new(),
+        };
     }
 
-    fn generate_index(&mut self, root_dir: PathBuf, section: &char) {
-        self.index.clear();
-        fn traverse_directory(
-            index: &mut Node,
-            current_dir: &PathBuf,
-            extension_node: &mut Node,
-            section: &char,
-        ) {
-            if current_dir.metadata().is_err() || fs::read_dir(&current_dir).is_err() {
-                return;
-            }
+    fn get_index(&self) -> &Vec<PathBuf> {
+        &self.indexed_files
+    }
 
-            let entries = fs::read_dir(current_dir).expect("Failed to read directory");
-            for entry in entries {
-                let entry = entry.expect("Failed to get entry");
-                if entry.file_type().unwrap().is_dir() {
-                    traverse_directory(index, &entry.path(), extension_node, section);
-                } else if entry.file_type().unwrap().is_file() {
-                    let file_name = entry.file_name();
-                    let file_name_str = match file_name.to_str() {
-                        Some(s) => s,
-                        None => continue,
-                    };
-                    let mut dot = true;
-                    let path = entry.path();
-                    if *section == '.' {
-                        dot = false;
-                    }
-                    if file_name_str.starts_with(*section) || *section == '*' || *section == '.' {
-                        if !file_name_str.starts_with('.') && dot {
-                            index.insert(file_name_str, path.clone());
-                        }
-                        let mut file_name = file_name_str.to_string();
-                        file_name.remove(0);
+    fn set_root_dir(&mut self, root_dir: PathBuf) {
+        self.root_dir = root_dir;
+    }
 
-                        // generate new node based on file extension
-                        let extension = path
-                            .extension()
-                            .and_then(OsStr::to_str)
-                            .unwrap_or(&file_name);
-                        extension_node.insert(extension, path.clone());
-                    }
-                }
+    fn get_root_dir(&self) -> &PathBuf {
+        &self.root_dir
+    }
+
+    fn search(&self, key: &String) -> Vec<PathBuf> {
+        let mut found = Vec::new();
+        let regex = Regex::new(&key).unwrap_or(Regex::new("None").unwrap());
+
+        for file in &self.indexed_files {
+            if regex.is_match(file.file_name().unwrap().to_str().unwrap()) {
+                found.push(file.clone());
             }
         }
-        let mut node = Node::new();
-        traverse_directory(&mut self.index, &root_dir, &mut node, section);
-        self.index.add_value('.', node);
+        found
     }
 }
 
-pub struct Search {
-    index: Node,
-    search_part: char,
-}
 #[cfg(test)]
 mod tests {
-    use std::env;
-
-    use env::current_dir;
-
     use super::*;
+
+    #[test]
+    fn test_new() {
+        let search = Search::new();
+        assert!(search.indexed_files.is_empty());
+        assert_eq!(search.root_dir, PathBuf::from("C:\\"));
+    }
+
+    #[test]
+    fn test_set_root_dir() {
+        let mut search = Search::new();
+        let new_root = PathBuf::from("D:\\");
+        search.set_root_dir(new_root.clone());
+        assert_eq!(search.root_dir, new_root);
+    }
 
     #[test]
     fn test_generate_index() {
         let mut search = Search::new();
-        let test_dir = current_dir().unwrap();
-        search.generate_index(test_dir, &'*');
-        assert!(!search.index.is_empty());
-    }
-
-    #[test]
-    fn test_search() {
-        let mut search = Search::new();
-        let test_dir = current_dir().unwrap();
-        search.generate_index(test_dir, &'*');
-        let keyword = String::from("main.rs");
-        let result = search.search(&keyword);
-        assert!(result.is_ok());
-        assert!(!result.unwrap().is_empty());
+        search.set_root_dir(PathBuf::from("."));
+        search.generate_index();
+        assert!(!search.indexed_files.is_empty());
     }
 
     #[test]
     fn test_save_and_load_index() {
         let mut search = Search::new();
-        let test_dir = current_dir().unwrap();
-        search.generate_index(test_dir, &'*');
+        search.set_root_dir(PathBuf::from("."));
+        search.generate_index();
         search.save_index();
 
         let mut new_search = Search::new();
-        new_search.load_index('m'); // Assuming 'a' is a valid section
-        assert!(!new_search.index.is_empty());
+        new_search.set_root_dir(PathBuf::from("."));
+        new_search.load_index();
+        assert_eq!(search.indexed_files, new_search.indexed_files);
+    }
+
+    #[test]
+    fn test_get_index() {
+        let mut search = Search::new();
+        search.set_root_dir(PathBuf::from("."));
+        search.generate_index();
+        let index = search.get_index();
+        assert_eq!(index, &search.indexed_files);
     }
 }
